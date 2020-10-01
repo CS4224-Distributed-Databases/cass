a890dda93cbaa01e93a0aa496e9fa747ed1f887e
KerrynEer <39527245+KerrynEer@users.noreply.github.com>
2020-10-02 01:27:02 +0800

    Add performance measurement logic

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,12 +4,14 @@ import Transactions.*;
 
 import com.datastax.driver.core.*;
 
-import java.util.HashMap;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
     private static Cluster cluster;
+    private static final double convertSecondsDenom = 1000000000.0;
+    private static final double convertMilliSecondsDenom = 1000000.0;
+
     public static void main(String[] args) throws Exception {
 
         // (1) Initialise Cluster
@@ -56,6 +58,14 @@ public class Main {
         // (4) Take in inputs...parser from stdin redirection.
         HashMap<String, PreparedStatement> insertPrepared = new HashMap<>();
         Scanner sc = new Scanner(System.in);
+        int numOfTransactions = 0;
+        long startTime;
+        long endTime;
+        long transactionStart;
+        long transactionEnd;
+        List<Long> latencies = new ArrayList<>();
+
+        startTime = System.nanoTime();
         while (sc.hasNext()) {
             String inputLine = sc.nextLine();
             BaseTransaction transaction = null;
@@ -79,12 +89,65 @@ public class Main {
             }
 
             if (transaction != null) {
+                numOfTransactions++;
                 transaction.parseInput(sc, inputLine);
+                //CHECK IF WE WANT TO INCLUDE parseInput time as well?
+                transactionStart = System.nanoTime();
                 transaction.execute();
+                transactionEnd = System.nanoTime();
+                latencies.add(transactionEnd - transactionStart);
             }
         }
+        endTime = System.nanoTime();
+        long timeElapsed = endTime - startTime;
+        double timeElapsedInSeconds = timeElapsed/convertSecondsDenom;
+        Collections.sort(latencies);
+        double averageLatencyInMs = getAverageLatency(latencies)/convertMilliSecondsDenom;
+        double medianLatencyInMs = getMedianLatency(latencies)/convertMilliSecondsDenom;
+        double percentileLatency95InMs = getPercentileLatency(latencies, 95)/convertMilliSecondsDenom;
+        double percentileLatency99InMs = getPercentileLatency(latencies, 99)/convertMilliSecondsDenom;
+
+        printPerformance(numOfTransactions, timeElapsedInSeconds, averageLatencyInMs, medianLatencyInMs, percentileLatency95InMs, percentileLatency99InMs);
 
         close();
+    }
+
+    private static double getAverageLatency(List<Long> latencies) {
+        double sum = 0.0;
+        for(Long latency: latencies) {
+            sum += latency;
+        }
+        return sum/latencies.size();
+    }
+
+    private static double getMedianLatency(List<Long> latencies) {
+        int length = latencies.size();
+        double medianValue;
+        int index = length/2;
+        if (length%2 == 0) {
+            medianValue = latencies.get(index) + (latencies.get(index+1) - latencies.get(index))/2.0; //avoid overflow
+        } else {
+            medianValue = latencies.get(index);
+        }
+        return medianValue;
+    }
+
+    private static long getPercentileLatency(List<Long> latencies, int percentile) {
+        int length = latencies.size();
+        int index = length*(percentile/100);
+        return latencies.get(index);
+    }
+
+    private static void printPerformance(int numOfTransactions, double timeElapsedInSeconds, double averageLatencyInMs, double medianLatencyInMs, double percentileLatency95InMs, double percentileLatency99InMs) {
+        System.out.println("---------------- Performance Output ----------------");
+        System.out.println("Number of executed transactions: " + numOfTransactions);
+        System.out.println(String.format("Total transaction execution time (sec): %.2f", timeElapsedInSeconds));
+        System.out.println(String.format("Transaction throughput: %.2f", numOfTransactions/timeElapsedInSeconds));
+        System.out.println(String.format("Average transaction latency (ms): %.2f", averageLatencyInMs));
+        System.out.println(String.format("Median transaction latency (ms): %.2f", medianLatencyInMs));
+        System.out.println(String.format("95th percentile transaction latency (ms): %.2f", percentileLatency95InMs));
+        System.out.println(String.format("99th percentile transaction latency (ms): %.2f", percentileLatency99InMs));
+        System.out.println("----------------------------------------------------");
     }
 
     public static void close(){
