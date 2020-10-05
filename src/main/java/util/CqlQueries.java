d8f75dc54f8f5841b8dda344a943a0c26646a751
KerrynEer <39527245+KerrynEer@users.noreply.github.com>
2020-10-06 01:29:53 +0800

    Create Cql Queries for popular item transaction

--- src/main/java/util/CqlQueries.java
+++ src/main/java/util/CqlQueries.java
@@ -100,4 +100,20 @@ public class CqlQueries {
     public static String GET_ITEM_NUMBER_FROM_ORDER_LINE = "SELECT OL_I_ID from Order_Line WHERE OL_W_ID = ? AND OL_D_ID = ? AND OL_O_ID = ?";
     public static String GET_CUS_LIST_FROM_ITEM = "SELECT I_O_ID_LIST from Item WHERE I_ID = ?";
 
+    // -------------POPULAR ITEM TRANSACTION ----------------------------------------------------------------------------
+    //Uses Order Small instead of Order
+    public static String I_GET_DISTRICT_NEXT_ORDER_NUM = "SELECT D_NEXT_O_ID from District WHERE D_W_ID = ? AND D_ID = ?";
+    public static String I_GET_ORDER_INFO = "SELECT O_ENTRY, O_C_FIRST, O_C_MIDDLE, O_C_LAST FROM Order_Small WHERE O_W_ID = ? AND O_D_ID = ? AND O_ID = ?";
+    public static String I_GET_ORDER_LINES = "SELECT OL_I_ID, OL_I_NAME, OL_QUANTITY FROM Order_Line WHERE OL_W_ID = ? AND OL_D_ID = ? AND OL_O_ID = ?";
+
+    // Indexes for Popular Item Transaction
+    public static final int I_D_NEXT_O_ID_INDEX = 0;
+    public static final int I_O_ENTRY_INDEX = 0;
+    public static final int I_O_C_FIRST_INDEX = 1;
+    public static final int I_O_C_MIDDLE_INDEX = 2;
+    public static final int I_O_C_LAST_INDEX = 3;
+    public static final int I_OL_I_ID_INDEX = 0;
+    public static final int I_OL_I_NAME_INDEX = 1;
+    public static final int I_OL_QUANTITY_INDEX = 2;
+
 }
