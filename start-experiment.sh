eed4378ddb6e9aa147e6bd14b2de2313314c2385
KerrynEer <39527245+KerrynEer@users.noreply.github.com>
2020-10-19 18:01:27 +0800

    Create start-experiment.sh

---
+++ start-experiment.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+runProject() {
+  # Remove old log files on each server.
+  for ((i=0; i<5; i++)); do
+    server="xcnd$((20 + $i % 5))"
+    ssh cs4224j@$server.comp.nus.edu.sg "cd cass && rm -rf log && mkdir log" 
+    echo "Remove old logs"
+  done
+  
+  # $1 is consistencyLevel
+  # $2 is number of clients
+  
+  #iterate through each client and assign to the correct server to run
+  for ((i=1; i<=$2; i++)); do
+	server="xcnd$((20 + $i % 5))"
+	echo "Assign client $i on server $server"
+	
+	input_file="src\main\java\DataSource\xact-files\$i.txt"
+	stdout_file="log/$i.out.log"
+	stderr_file="log/$i.err.log"
+	echo "commented out running"
+	ssh $server "cd cass && java -Xms45g -Xmx45g -cp target/*:target/dependency/*:. Main ${1} < ${input_file} > ${stdout_file} 2> ${stderr_file} &" > /dev/null 2>&1 &
+	
+	echo "Finish running $i transaction file on $server"
+  done
+}
+
+buildProject() {
+  for ((i=1; i<=5; i++)); do
+    server="xcnd$((20 + $i % 5))"
+    ssh cs4224j@$server.comp.nus.edu.sg "cd cass && mvn clean dependency:copy-dependencies package"
+    echo "Built project on server ${server}."
+  done
+}
+
+echo "Starting to build on all servers"
+buildProject
+echo "Starting to run project with $1 consistency level and $2 instances"
+runProject $1 $2
+echo "Complete experiment"
