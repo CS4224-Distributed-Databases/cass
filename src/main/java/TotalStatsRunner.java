2a50e5c183a9a947255f4d217f35c3a44d0a0ac4
KerrynEer <39527245+KerrynEer@users.noreply.github.com>
2020-10-27 12:46:15 +0800

    Update TotalStats to output client_stats csv too

--- src/main/java/TotalStatsRunner.java
+++ src/main/java/TotalStatsRunner.java
@@ -1,9 +1,15 @@
 import com.datastax.driver.core.*;
+import util.StatsItem;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+// This class iterates through the err log files
+// Get the stats for max, min, avg throughputs among all clients for this experiment -> ultimately for throughputs.csv
+// Get all clients performance stats for this experiment -> ultimately for clients.csv
 public class TotalStatsRunner {
 
     private static Cluster cluster;
@@ -52,17 +58,17 @@ public class TotalStatsRunner {
                         .getProtocolOptions()
                         .getProtocolVersion());
 
-        // Iterate through the err log files and get the stats for max, min, avg throughputs among all clients
-
         // USER TO INPUT THE NUM OF CLIENTS THAT WAS PASSED IN TO TEST
         int numClients = Integer.parseInt(args[1]);
-        double[] result = getThroughputsStatsFromLogFiles(numClients);
+        List<StatsItem> clientsStatsList = new ArrayList<>();
+        double[] result = getStatsFromLogFiles(numClients, clientsStatsList);
         writeTotalThroughputStatsToCsv(result[0], result[1], result[2], numClients);
+        writeClientsStatsToCsv(clientsStatsList);
 
         close();
     }
 
-    public static double[] getThroughputsStatsFromLogFiles(int numClients) {
+    public static double[] getStatsFromLogFiles(int numClients, List<StatsItem> clientsStatsList) {
         double[] results = new double[3]; // first store min, second store max, third store total
 
         double minThroughputPercentage = Double.MAX_VALUE;
@@ -71,21 +77,62 @@ public class TotalStatsRunner {
 
         for (int i = 1; i <= numClients; i++) {
             String fileName = "log/" + i + ".err.log";
+            StatsItem statsForThisClient = new StatsItem();
+            statsForThisClient.clientNum = (double) i;
             try {
                 BufferedReader br = new BufferedReader(new FileReader(fileName));
                 String line;
                 while( (line = br.readLine() ) != null) {
-                    if (line.startsWith("Transaction throughput: ")) {
+                    if (line.startsWith("Number of executed transactions: ")) {
+                        Pattern p = Pattern.compile("(\\d+(?:\\.\\d+))");
+                        Matcher m = p.matcher(line);
+                        if (m.find()) {
+                            statsForThisClient.numOfTransactions = Double.parseDouble(m.group(1));
+                        }
+                    } else if (line.startsWith("Total transaction execution time (sec): ")) {
+                        Pattern p = Pattern.compile("(\\d+(?:\\.\\d+))");
+                        Matcher m = p.matcher(line);
+                        if (m.find()) {
+                            statsForThisClient.executionTime = Double.parseDouble(m.group(1));
+                        }
+                    } else if (line.startsWith("Transaction throughput: ")) {
                         Pattern p = Pattern.compile("(\\d+(?:\\.\\d+))");
                         Matcher m = p.matcher(line);
                         if (m.find()) {
                             double throughput = Double.parseDouble(m.group(1));
+                            statsForThisClient.throughput = throughput;
                             minThroughputPercentage = Math.min(minThroughputPercentage, throughput);
                             maxThroughputPercentage = Math.max(maxThroughputPercentage, throughput);
                             totalThroughputPercentage += throughput;
                         }
+                    } else if (line.startsWith("Average transaction latency (ms): ")) {
+                        Pattern p = Pattern.compile("(\\d+(?:\\.\\d+))");
+                        Matcher m = p.matcher(line);
+                        if (m.find()) {
+                            statsForThisClient.avgLatency = Double.parseDouble(m.group(1));
+                        }
+                    } else if (line.startsWith("Median transaction latency (ms): ")) {
+                        Pattern p = Pattern.compile("(\\d+(?:\\.\\d+))");
+                        Matcher m = p.matcher(line);
+                        if (m.find()) {
+                            statsForThisClient.medianLatency = Double.parseDouble(m.group(1));
+                        }
+                    } else if (line.startsWith("95th percentile transaction latency (ms): ")) {
+                        Pattern p = Pattern.compile("(\\d+(?:\\.\\d+))");
+                        Matcher m = p.matcher(line);
+                        if (m.find()) {
+                            statsForThisClient.latency95 = Double.parseDouble(m.group(1));
+                        }
+                    } else if (line.startsWith("99th percentile transaction latency (ms): ")) {
+                        Pattern p = Pattern.compile("(\\d+(?:\\.\\d+))");
+                        Matcher m = p.matcher(line);
+                        if (m.find()) {
+                            statsForThisClient.latency99 = Double.parseDouble(m.group(1));
+                        }
                     }
                 }
+                clientsStatsList.add(statsForThisClient);
+
             } catch (IOException e) {
                 e.getMessage();
             }
@@ -124,6 +171,62 @@ public class TotalStatsRunner {
             writer.write(sb.toString());
 
             System.out.println("done writing to output/throughput_stats.csv");
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+
+    public static void writeClientsStatsToCsv(List<StatsItem> clientStatsList) {
+        /*
+            CLIENT STATS
+         */
+        try (PrintWriter writer = new PrintWriter(new File("src/output/client_stats.csv"))) {
+            StringBuilder sb = new StringBuilder();
+            // Key in the experiment number manually in a separate csv
+            // 1-4 for Cassandra, 5-8 for Cockroach
+//          sb.append("experiment_number");
+//          sb.append(',');
+            sb.append("client_number");
+            sb.append(',');
+            sb.append("Number of executed transactions");
+            sb.append(',');
+            sb.append("Total transaction execution time (sec)");
+            sb.append(',');
+            sb.append("Transaction throughput");
+            sb.append(',');
+            sb.append("Average transaction latency (ms)");
+            sb.append(',');
+            sb.append("Median transaction latency (ms)");
+            sb.append(',');
+            sb.append("95th percentile transaction latency (ms)");
+            sb.append(',');
+            sb.append("99th percentile transaction latency (ms)");
+            sb.append('\n');
+
+            for (StatsItem statsResult : clientStatsList) {
+                sb.append(statsResult.clientNum);
+                sb.append(',');
+                sb.append(statsResult.numOfTransactions);
+                sb.append(',');
+                sb.append(statsResult.executionTime);
+                sb.append(',');
+                sb.append(statsResult.throughput);
+                sb.append(',');
+                sb.append(statsResult.avgLatency);
+                sb.append(',');
+                sb.append(statsResult.medianLatency);
+                sb.append(',');
+                sb.append(statsResult.latency95);
+                sb.append(',');
+                sb.append(statsResult.latency99);
+                sb.append(',');
+                sb.append('\n');
+            }
+
+            writer.write(sb.toString());
+
+            System.out.println("done writing to output/client_stats.csv");
         } catch (FileNotFoundException e) {
             System.out.println(e.getMessage());
         }
